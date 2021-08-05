c66b321db5512830e32b00e65f0266702f9683fa
educafe <krdiyoon@gmail.com>
2021-08-05 23:38:04 +0900

    Updated on 2021-09-06

---
+++ 08.tcp_ip/04.tcp_no_bound_data/01.hello_client.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[]){
+	int sock;
+	struct sockaddr_in serv_addr;
+	socklen_t addr_size;
+	int len;
+
+	char message1[]="Hello world";
+	char message2[]="Good morning";
+	char message3[]="How are you";
+
+	if(argc!=3){
+		printf("Usage : %s <IP> <port>\n", argv[0]);
+		exit(1);
+	}
+	
+	sock=socket(PF_INET, SOCK_STREAM, 0);
+	if(sock == -1) 
+	{
+		perror("socket() error!!");
+		exit(1);
+	}
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family=AF_INET;
+	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
+	serv_addr.sin_port=htons(atoi(argv[2]));
+
+	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1){
+		perror("connect() error!!");
+		exit(1);
+	}
+	
+	len = send(sock, message1, sizeof(message1), 0);
+	printf("result1 = %d\n", len);
+	len = send(sock, message2, sizeof(message2), 0);
+	printf("result1 = %d\n", len);
+	len = send(sock, message3, sizeof(message3), 0);
+	printf("result1 = %d\n", len);
+
+#if 1
+	printf("Enter to quit...");
+	getchar();
+#endif
+	close(sock);
+	return 0;
+}
